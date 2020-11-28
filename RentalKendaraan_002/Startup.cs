e22c8b1295e0e1e258e5314b91414df64d6ddd43
NaufalHaryanda <naufalharyanda10@gmail.com>
2020-11-28 13:30:51 +0700

    Upload File

--- RentalKendaraan_002/Startup.cs
+++ RentalKendaraan_002/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace RentalKendaraan_002
 {
@@ -35,6 +37,7 @@ namespace RentalKendaraan_002
             services.AddDbContext<Models.RentKendaraanContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
